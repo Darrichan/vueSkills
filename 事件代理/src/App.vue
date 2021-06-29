dff39bc38edb047a64ec2b808eb859f2b571b78b
darrichan <770543388@qq.com>
2021-06-29 15:57:44 +0800

    eventProxy

---
+++ 事件代理/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div @click="handleClick">
+      <ul>
+        <li v-for="(item,index) in prj"
+            :key="index"
+            :dataIndex="index">{{item}}</li>
+      </ul>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      prj: ['项目1', '项目2', '项目3']
+    }
+  },
+  name: 'App',
+  methods: {
+    handleClick (e) {
+      console.log('index'+ e.target.getAttribute('dataindex'));
+    }
+  }
+}
+</script>
+
+<style>
+</style>
